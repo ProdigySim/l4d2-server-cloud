0284f2d5f66bd6393e9b79556310909079a08c18
ProdigySim <prodigysim@gmail.com>
2025-05-12 20:22:49 -0500

    Update to latest providers

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 terraform {
-required_version = ">= 0.14.0"
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
-      version = "~> 1.53.0"
+      version = "~> 3.0.0"
     }
   }
 }
