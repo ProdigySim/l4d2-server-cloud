243cabbdd76e503e85a334544402eb2786f29c64
ProdigySim <prodigysim@gmail.com>
2025-05-24 11:12:56 -0500

    Setup initial server with ssh access

--- main.tf
+++ main.tf
@@ -16,3 +16,42 @@ resource "openstack_compute_keypair_v2" "test-keypair" {
   name       = "psim_alpc"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGHhx+SvinGWrZZrtjB6MjdnhhO0+hCdRR1b1NuajOQ+LUKOJiVXYaAzLEKsL8YO8UGNANuscz4PTs4TPOKk5tQFT7AHN4x/7n/bqISxQ7Lj8BkekRTxtq+kil0e19uobB8xldQiHYauxuB2/tRN/JRSUMIN2c4ARX1xJ5fBD9xYaUrEzXy27EKpUGV0DGPRGIInpdMLinuJZvLpyjLYb1MkmzOYqpQVZdvDvs7B6wdIg/wPsz0aWqpcJXXEznQKME7IomZNEsB+ERnFwj6XOteJGoHUQsfItjXU9C1z+AZC5gMdpsOcPD4pGtErp01ECAicjCvI/o6wOJ90drJ9QP"
 }
+
+# Honestly just find any network...
+data "openstack_networking_network_v2" "network" {
+  name = "public"
+}
+
+data "openstack_compute_flavor_v2" "m2new" {
+  name = "M2-new"
+}
+
+resource "openstack_networking_secgroup_v2" "ssh-incoming" {
+  name        = "ssh-incoming"
+  description = "Allow SSH incoming from just my IP"
+}
+resource "openstack_networking_secgroup_rule_v2" "ssh-incoming-rule" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "${var.ssh_allowed_ip}/32"
+  security_group_id = openstack_networking_secgroup_v2.ssh-incoming.id
+}
+
+resource "openstack_compute_instance_v2" "basic" {
+  name            = "basic"
+  image_name      = "ubuntu-24.04-x86_64"
+  flavor_id       = data.openstack_compute_flavor_v2.m2new.id
+  key_pair        = openstack_compute_keypair_v2.test-keypair.id
+  security_groups = ["default", openstack_networking_secgroup_v2.ssh-incoming.name]
+
+  metadata = {
+    this = "that"
+  }
+
+  network {
+    name = data.openstack_networking_network_v2.network.name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "ssh_allowed_ip" {
+  type        = string
+  sensitive   = true
+  description = "Allow this IP to SSH"
+}
